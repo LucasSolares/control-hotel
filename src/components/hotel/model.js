b37f05b39c266b25ca97d2c46233e9dd4163d55f
Lucas Solares <solareslucas.figueroa@gmail.com>
2020-07-02 17:48:24 -0600

    Add default image for hotel

--- src/components/hotel/model.js
+++ src/components/hotel/model.js
@@ -10,6 +10,7 @@ const schema = new Schema({
     direction: String,
     price: Number,
     availability: Schema.Types.Date,
+    imageURL: { required: true, default: 'https://www.plazapublica.com.gt/sites/default/files/hotel_radisson.jpg', type: String },
     qualification: { type: Number, required: false, default: 0 },
 
 })
